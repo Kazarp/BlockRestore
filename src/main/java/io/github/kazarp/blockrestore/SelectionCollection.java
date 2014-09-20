d06fd8a11ddf83db315984e5a364612101406ba3
Dennis Pražák <prazak.dennis@gmail.com>
2014-09-20 12:28:14 +0200

    Repaired methods setBlock1 and setBlock2

--- src/main/java/io/github/kazarp/blockrestore/SelectionCollection.java
+++ src/main/java/io/github/kazarp/blockrestore/SelectionCollection.java
@@ -9,42 +9,38 @@ import org.bukkit.entity.Player;
 public class SelectionCollection {
 	static List<Selection> selections = new ArrayList<Selection>();
 	
-	public static Selection getSelectionByPlayer(Player p){
+	public static void setBlock1(Player p, Block b1){
 		for(int i = 0; i < selections.size(); i++){
-			if(Selection.comparePlayers(selections.get(i).getPlayer(), p)){
-				return selections.get(i);
+			if(Selection.comparePlayers(p, selections.get(i).getPlayer())){
+				selections.get(i).setBlock1(b1);
+				Message.send("LEFT 0", p);
+				return;
 			}
 		}
-		return null;
-	}
-	
-	public static void setBlock1(Player p, Block b1){
-		//TODO implement
-		// If is the Player in the collection, set his b1, else add new record
+		add(p, b1, null);
+		Message.send("LEFT 1", p);
 	}
 	public static void setBlock2(Player p, Block b2){
-		//TODO implement
+		for(int i = 0; i < selections.size(); i++){
+			if(Selection.comparePlayers(p, selections.get(i).getPlayer())){
+				selections.get(i).setBlock1(b2);
+				Message.send("RIGHT 0", p);
+				return;
+			}
+		}
+		add(p, null, b2);
+		Message.send("RIGHT 1", p);
 	}
 	
+	// "add" Methods' overloads
 	public static void add(Selection s){
-		Selection testS = getSelectionByPlayer(s.getPlayer());
-		if(testS != null){
-			selections.set(selections.indexOf(testS), s);
-		}
-		else{
-			selections.add(s);
-		}
+		selections.add(s);
 	}
 	public static void add(Player p, Block b1, Block b2){
 		add(new Selection(p, b1, b2));
 	}
 	public static void add(Player p, int x1, int y1, int z1, int x2, int y2, int z2){
 		add(new Selection(p, x1, y1, z1, x2 ,y2, z2));
-	}
-	
-	
-	public static boolean containsPlayer(Player p){
-		return getSelectionByPlayer(p) != null;
 	}
 	public static void clear(){
 		selections.clear();
